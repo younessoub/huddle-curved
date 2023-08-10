683b4775bbf1d122434e7533bd8a0ff0c9617547
Youness Oubelkacem <78394489+younessoub@users.noreply.github.com>
2023-08-10 18:06:04 +0000

    added animations

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const divs = document.querySelectorAll('.feature-main>div');
 
 function reveal() {
-
+    divs.forEach(e=>{
+        // console.log(e.getBoundingClientRect().top)
+        if(window.innerHeight - e.getBoundingClientRect().top > 150){
+            e.classList.add('reveal')
+        }
+    })
 }
 
 document.addEventListener('scroll', reveal)
